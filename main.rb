57fcce8be9a071c2857638327295501d8b4aad26
kasia-misirli <kasia.misirli@gmail.com>
2018-06-12 23:06:59 +1000

    adding happy path tests and page summary

--- main.rb
+++ main.rb
@@ -3,22 +3,17 @@ require 'sinatra/reloader'
 require 'pry'
 require 'httparty'
 
-
-
-def reverse string
-  string.each_char.to_a.reverse.join
-end
-
 get '/' do
   erb(:home)
 end
 
-
 get '/api' do
-  url= "https://kasiatest.zendesk.com/api/v2/tickets.json?&per_page=25&page=#{params[:page]}"
+  url = "https://kasiatest.zendesk.com/api/v2/tickets.json?&per_page=25&page=#{params[:page]}"  
   auth = {:username => "kasiazendesk@gmail.com", :password => "zendesk2012"}
   result = HTTParty.get(url,:basic_auth => auth)
   @tickets = result.parsed_response["tickets"]
+  @page = (params[:page].to_i)
+  @alltickets = result["count"]
   erb (:index)
 end
 
@@ -30,6 +25,8 @@ get '/api/:id' do
   erb (:single)
 end
 
-
-
+not_found do
+  status 404
+  erb(:error)
+end
 
--- main_spec.rb
+++ main_spec.rb
@@ -8,19 +8,23 @@ def app
 end
 
 describe "testing api" do
+  before(:all) do
+    url = "https://kasiatest.zendesk.com/api/v2/tickets.json?&per_page=25&page=1"  
+    auth = {:username => "kasiazendesk@gmail.com", :password => "zendesk2012"}
+    @result = HTTParty.get(url,:basic_auth => auth)
+  end
   include Rack::Test::Methods
-  it "should load the home page" do
-    get '/'
-    last_response.status.should == 200
+  it "returns data" do
+    expect(@result["tickets"]).to be_truthy
   end
-  it "should load the api page" do
-    get '/api'
-    last_response.status.should == 200
+  it "returns array of 25 tickets per page" do
+    expect(@result["tickets"].count).to eql(25)
   end
-  it "should return json" do
-    get '/api'
-    json = JSON.parse(response.body)
-    expect(json['api']).to eql('ok')
+  it "returns total 101 tickets" do
+    expect(@result["count"]).to eql(101)
+  end
+  it "returns 35 items per ticket" do
+    expect(@result["tickets"][0].count).to eql(35)
   end
 end
 
